59dadb838c8a3a1191a3d0d1bfbda1011038d3e7
ryonsd <ryo.nishida.24@gmail.com>
2022-12-26 13:44:19 +0900

    changed names of directories

--- crowdwalk-gym/CrowdWalkGym/tools/get_state_reward.py
+++ crowdwalk-gym/CrowdWalkGym/tools/get_state_reward.py
@@ -5,20 +5,24 @@ import pandas as pd
 import os
 import json
 import sys
-import math
 
-def get_reward(env, log, step_s, step_e, log_dir, n_obj, sample_t = 30):
+def get_state_reward(env, log, step_duration, step, log_dir, n_obj, sample_t = 30):
+
+    # リンク上の各時刻の歩行者数および密度
     link_dict = {} # length, width, density of each link
+    next_state_dict = {} # number of pedestrians at each link
 
-    step_duration = step_e - step_s
     T = int(step_duration / sample_t)
     link_dict = {}
+    next_state_dict = {}
     for link_name, link_attribute in env.link.items():
         if link_name != "generation_link":
-            link_dict[link_name] = {"link_list": link_attribute["id"], "length": link_attribute["length"], "width": link_attribute["width"], 
+            link_dict[link_name] = {"link_list": link_attribute["id"], 
+                                    "length": link_attribute["length"], "width": link_attribute["width"], 
                                     "n_ped": np.zeros(T), "density": np.zeros(T)}
+        next_state_dict[link_name] = 0
 
-    check_step = np.arange(step_s+sample_t, step_e, sample_t)
+    check_step = np.linspace(step - step_duration + sample_t, step, T)
 
     for t, s in enumerate(check_step):
         log_sample = log[log.current_traveling_period == s].reset_index(drop=True)
@@ -31,7 +35,11 @@ def get_reward(env, log, step_s, step_e, log_dir, n_obj, sample_t = 30):
                     if l in link_attribute["link_list"]:
                         link_attribute["n_ped"][t] += 1
                         link_attribute["density"][t] += 1 / (link_attribute["length"]* link_attribute["width"])
-    
+                        if t == T-1:
+                            next_state_dict[link_name] += 1
+
+    next_state = list(next_state_dict.values())
+
     # congestion_degree
     congestion_degree = 0
     for link_name, link_attribute in link_dict.items():
@@ -51,6 +59,7 @@ def get_reward(env, log, step_s, step_e, log_dir, n_obj, sample_t = 30):
         # if the agent has arrived
         route = np.nan 
         distance = np.nan
+        
         if agent_log[i][-1] == "arrived":
             
             agent_id = agent_log[i][0]
@@ -78,12 +87,12 @@ def get_reward(env, log, step_s, step_e, log_dir, n_obj, sample_t = 30):
     if n_obj == 1:
         reward = -int(congestion_degree)
     elif n_obj == 2:
-        reward = [int(travel_distance), -int(congestion_degree)]
+        reward = [int(travel_distance)/50, -int(congestion_degree)/100]
 
-    done = True
+    done = False
 
-    return reward, done
-
+    return next_state, reward, done
+    
 
 
 if __name__ == '__main__':
@@ -93,9 +102,9 @@ if __name__ == '__main__':
     path_to_gym = args[1]
     env_name = args[2]
     step = int(args[3])
-    sim_previous_step = int(args[4])
-    sim_final_step = int(args[5])
-    sim_log_dir = args[6] 
+    step_duration = int(args[4])
+    sim_step = int(args[5])
+    sim_dir = args[6] 
     agent_log_dir = args[7]
     n_obj = int(args[8])
 
@@ -111,18 +120,26 @@ if __name__ == '__main__':
     elif env_name == "moji_small":
         env = MojiSmallEnv()
 
-    log = pd.read_csv(agent_log_dir+ "/log_individual_pedestrians.csv")
+    log = pd.read_csv(agent_log_dir + "/log_individual_pedestrians.csv")
+    next_state_, reward, done = get_state_reward(env, log, step_duration, sim_step, agent_log_dir, n_obj)
 
-    reward, done = get_reward(env, log, sim_previous_step, sim_final_step, agent_log_dir, n_obj)
+    gen = pd.read_csv(sim_dir + "/generation.csv")
+    next_generation_pedestrian_number = gen[gen.step == (step+1)]["n_ped"].values[0]
+
+    next_state = [step+1, int(next_generation_pedestrian_number)]
+    next_state.extend(next_state_)
+    
 
     with open(agent_log_dir + "/history.json", "r") as f:
         history = json.load(f)
 
     history[str(step)]["reward"] = reward
-    next_state = [step+1]
-    next_state.extend(list(np.zeros(env.nS+1)))
     history[str(step)]["next_state"] = next_state
     history[str(step)]["done"] = done
+    history[str(step+1)] = {"sim_step": sim_step, "state": next_state, "action": np.nan, "reward": np.nan, "next_state": np.nan, "done": np.nan}
 
     with open(agent_log_dir + "/history.json", "w") as f:
         json.dump(history, f, indent=2)
+
+
+    
